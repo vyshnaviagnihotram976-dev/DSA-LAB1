af8018c056f05ab7fa101f5f43d73288b567d916
vyshnaviagnihotram976-dev <vyshnaviagnihotram976@gmail.com>
2025-11-22 12:34:30 +0530

    QUICK SORT
    Implement Quick Sort algorithm with partitioning and swapping functions.

---
+++ Quick.c
@@ -0,0 +1,59 @@
+//Aim:TO Implement Quick sort 
+include <stdio.h>
+
+//Swap the numbers / Call by referene to persist changes
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+// Partition 
+int partition(int list[], int low, int high)
+{
+    int pivot = list[high];
+    int i = low - 1;
+
+    for (int j = low; j < high; j++)
+    {
+        if (list[j] < pivot)
+        {
+            i++;
+            swap(&list[i], &list[j]);
+        }
+    }
+    swap(&list[i + 1], &list[high]);
+    return i + 1;
+}
+
+// Quick Sort function
+void quickSort(int list[], int low, int high)
+{
+    if (low < high)
+    {
+        int pi = partition(list, low, high);
+        quickSort(list, low, pi - 1);
+        quickSort(list, pi + 1, high);
+    }
+}
+
+int main()
+{
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int list[n];
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++)
+        scanf("%d", &list[i]);
+
+    quickSort(list, 0, n - 1);
+
+    printf("Sorted array (Quick Sort): ");
+    for (int i = 0; i < n; i++)
+        printf("%d ", list[i]);
+
+    return 0;
+}
